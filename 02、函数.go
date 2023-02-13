0114964d58fb0c0c0711e9795ed61dddbb6040db
南宫乘风 <1794748404@qq.com>
2023-02-13 09:34:06 +0800

    增加结构体的学习

--- 02、函数.go
+++ 02、函数.go
@@ -24,7 +24,70 @@ func sum_num() {
 	fmt.Println(plus) // 和为：9
 	fmt.Println(sub)  // 差为：-1
 }
-func main() {
-	sum_num()
+
+//结构体
+type person struct {
+	name string
+	city string
+	age  int
+}
+
+func struct_name() {
+	var p1 person
+	p1.name = "张三"
+	p1.city = "北京"
+	p1.age = 18
+	fmt.Printf("p1=%v\n", p1)  // p1={张三 北京 18}
+	fmt.Printf("p1=%#v\n", p1) // p1=main.person{name:"张三", city:"北京", age:18}
+
+	var p2 = new(person)
+	p2.name = "张三"
+	p2.age = 20
+	p2.city = "北京"
+	fmt.Printf("p2=%#v \n", p2) //p2=&main.person{name:"张三", city:"北京", age:20}
+
+	p4 := person{
+		name: "zhangsan",
+		city: "北京",
+		age:  18,
+	}
+	// p4=main.person{name:"zhangsan", city:"北京", age:18}
+	fmt.Printf("p4=%#v\n", p4)
+}
+
+func struct_two() {
+	name_p := Person{
+		name: "heian",
+		age:  18,
+	}
+	fmt.Println("变量：", name_p)
+	name_p.printInfo()
 	fmt.Printf("全部变量：num=%d\n", num) //num=10
+}
+
+type Person struct {
+	name string
+	age  int8
+}
+
+//值类型接受者
+func (p Person) printInfo() {
+	fmt.Printf("姓名:%v 年龄:%v\n", p.name, p.age) // 姓名:小王子 年龄:25
+}
+
+//指针类型接收者
+func (p *Person) setInfo(name string, age int8) {
+	p.name = name
+	p.age = age
+}
+
+func main() {
+	//struct_name()
+	p1 := Person{
+		name: "小王子",
+		age:  25,
+	}
+	p1.printInfo() // 姓名:小王子 年龄:25
+	p1.setInfo("张三", 20)
+	p1.printInfo() // 姓名:张三 年龄:20
 }
